cdb593b235cb9e7baa66115675d07d096daa861f
windijcode <windijcode@outlook.com>
2021-02-07 13:42:30 +0700

    End of lesson Model 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  title = 'Template Driven Form';
+  topics = ['Angular', 'React', 'Vue'];
 }
